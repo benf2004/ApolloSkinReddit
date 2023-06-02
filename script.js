223ba8dfc69f17e7b3501b54530d92cc6418c289
benf2004 <12115686+benf2004@users.noreply.github.com>
2023-06-02 13:38:50 -0600

    setup

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// script.js
 
+// Function to inject CSS into the page
+function injectCSS(file) {
+    const link = document.createElement('link');
+    link.href = chrome.runtime.getURL(file);
+    link.rel = 'stylesheet';
+    console.log(link)
+    document.head.appendChild(link);
+}
+
+// Call the injectCSS function with the CSS file
+injectCSS('styles.css');
